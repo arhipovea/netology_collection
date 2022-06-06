3e8828820ab30ff799f74631bb9979c852938311
Evgeniy Arkhipov <arhipovea@gmail.com>
2022-06-06 14:09:20 +0300

    init

---
+++ lesson_collection/README.md
@@ -0,0 +1,3 @@
+# Ansible Collection - netology_mnt_13.lesson_collection
+
+Documentation for the collection.
---
+++ lesson_collection/plugins/modules/my_module.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2018, Terry Jones <terry.jones@example.org>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+from email.policy import default
+from fileinput import filename
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+---
+module: my_module
+
+short_description: Тестовый модуль для netology.
+
+version_added: "1.0.0"
+
+description: Тестовый модуль для netology, который создаёт файл по пути path, с именем filename и содержимым content.
+
+options:
+    path:
+        description: Путь до файла.
+        required: true
+        type: path
+    content:
+        description: Содержание файла.
+        required: true
+        type: str
+    filename:
+        description: Имя файла.
+        required: false
+        type: str
+        default: file
+
+extends_documentation_fragment:
+    - netology_collection.lesson_collection.my_module_doc
+
+author:
+    - Arkhipov Evgeniy (@arhipovea)
+'''
+
+EXAMPLES = r'''
+# Create file
+- name: Test with a file create
+  netology_mnt_13.lesson_collection.my_module:
+    path: ./
+    content: test content
+
+# Create file with filename new_filename
+- name: Test with a message and changed output
+  netology_mnt_13.lesson_collection.my_module:
+    path: /tmp
+    content: new content
+    filename: new_filename
+'''
+
+RETURN = r'''
+# These are examples of possible return values.
+msg:
+    description: The output message.
+    type: str
+    returned: always
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+import os
+
+
+def run_module():
+    module_args = dict(
+        path=dict(type='path', required=True, aliases=['dest', 'name']),
+        content=dict(type='str', required=True),
+        filename=dict(type='str', required=False, default='file')
+    )
+
+    result = dict(
+        changed=False,
+        msg='File created'
+    )
+
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=False
+    )
+
+    fullname = os.path.join(module.params['path'], module.params['filename'])
+
+    if os.path.isfile(fullname) and open(fullname, "r").read() == module.params['content']:
+        result['changed'] = False
+    else:
+        try:
+            with open(fullname, "w") as f:
+                f.write(module.params['content'])
+            result['changed'] = True
+        except IOError as e:
+            result['msg'] = e.strerror
+
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
